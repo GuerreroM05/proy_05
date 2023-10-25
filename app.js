27a3a6434aad7427666ae929235cac742c78f375
GuerreroM05 <daimerguerrerofuratena@gmail.com>
2023-10-25 13:23:26 -0500

    correccion

--- app.js
+++ app.js
@@ -1,53 +1,41 @@
+//se utiliza para agregar colores y estilos a las salidas te textos en la terminal.
 require('colors');
 
-const functions = require('./proy_modules/math')
-
-
-const main = async() => {
-  
-    console.log('*****************************************'.cyan);
-    console.log(`*            `.cyan, 'MAYUSCULAS'.bgRed, `               *`.cyan);
-    console.log('*****************************************'.cyan);
-    console.log(`*              `.cyan, "" + functions.mayusculas('jesus'), `                  *   `.cyan);
-    console.log('*****************************************'.cyan);
-    console.log(`*            `.cyan, 'MINUSCULAS'.bgRed, `               *`.cyan);
-    console.log('*****************************************'.cyan);
-    console.log(`*              `.cyan, "" + functions.minusculas('DAVID'), `                  *   `.cyan);
-    console.log('*****************************************'.cyan);
-    console.log('*****************************************'.cyan);
-    console.log(`*     `.cyan, 'MAYUSCULAS Y MINUSCULAS'.bgRed, `         *`.cyan);
-    console.log('*****************************************'.cyan);
-    console.log(`*            `.cyan, "" + functions.mayusculasyminusculas('DAVID', 'JESUS'), `             *   `.cyan);
-    console.log('*****************************************'.cyan);
-    console.log('*****************************************'.cyan);
-    console.log(`*     `.cyan, 'SE QUITA LA ULTIMA LETRA'.bgRed, `        *`.cyan);
-    console.log('*****************************************'.cyan);
-    let derecha = "DAIMER GUERRERO";
-
-    function quitarLetra1() {
-      if ( derecha.length > 0) {
-        derecha = derecha.slice(0, -1);
-        console.log( derecha);
-        quitarLetra1(); 
-      }
+//lo utilizamos para acceder a las exportaciones que se hayan definido en ese archivo y utilizarlas en el actual 
+const math =require('./proy_modules/math.js')
+//se utiliza para definir un punto de entrada de un programa 
+const main = async () => {
+//muestra un mensaje en la consola 
+    console.log('hola SENA CBA\n'.blue);
+//define la palabra con varios numeros 
+    const numeros = [1, 2, 800, 912, 1000, 1283, 2811];
+    //Utilizamos for para crear bucles y repetir una seccion de codigo varias veces 
+    for (const numero of numeros) {
+        //utilizamos la funcion para invertir el numero 
+        const invertidoComoNumero = math.invertirNumero(numero);
+        //funcion para invertir un numero y devolver su representacion invertida como una cadena 
+        const invertidoComoCadena =math.invertirNumeroComoCadena(numero);
+        //mensaje de salida que muestra informacion de un numero y su inversion como numero y cadena 
+        console.log("El número '%s' es '%s' invertido como nùmero, y ¹%s como cadena",
+                        numero,invertidoComoNumero,invertidoComoCadena);
+}
+//arreglo que almacena una coleccion de cadenas de texto
+const textos = ['Hola sena', 'Ficha 2798618', 'Analisis y desarrollo de sofware']
+//repite a traves del arreglo "textos" y realiza una operacion para cada cadena de texto 
+for(const texto of textos){
+    //utiliza la funcio "invertir texto" para obtener la version invertida de ua cadena de texto
+    const textoInvertido = math.invertirTexto(texto);
+    //imprime informacio sobre la inversion de un texto
+    console.log("el texto '%s' se invierte como '%s'", texto,textoInvertido);
+}
+//se utiliza para declarar una varible cuyo valor no cambiara una vez asignada 
+const datos = [1234567, 'ADSO 2798618', [2798618, 'ADSO'] ]
+for(let dato of datos){
+    //se utiliza para cambiar el tipo de dato de una variable
+    const datoConvertido = math.conversionDatos(dato);
+    //se utiliza para insertar el valor de una variable detro de la cadena de texto
+    console.log(`El resultado de coversion es : ${datoConvertido}`);
     }
-    quitarLetra1();
-    console.log('*****************************************'.cyan);
 }
+
 main();
-console.log('*****************************************'.cyan);
-    console.log(`*  `.cyan, 'SE QUITA LA PRIMERA LETRA'.bgRed, `          *`.cyan);
-    console.log('*****************************************'.cyan); 
-
-let alreves = "jesus david";
-
-function quitarLetra2() {
-  if (alreves.length > 0) {
-    console.log(alreves);
-    alreves = alreves.slice(1); 
-    quitarLetra2();
-  }
-}
-
-quitarLetra2();
-console.log('*****************************************'.cyan);
--- proy_modules/math.js
+++ proy_modules/math.js
@@ -1,22 +1,80 @@
-const functions = {}
+    // definimos la variable math como constante y de tipo objeto.
+    const math ={};
+    
+    //definimos la funcion flecha invertirNumero como constante con parametro numero
+const invertirNumero = numero => {
+    //definimos invertido como variable y de tipo numero
+    let invertido = 0;
+    //ciclo while que se repite mientras el numero sea diferente de 0
+    while (numero != 0) {
+        invertido =10 * invertido + numero % 10
+        numero = (Math.floor(numero/ 10))
+    }
+    //devuelve el valor "invertido"
+    return invertido
+};
+//funcion que invierte un numero y lo representa como un texto                                                                                                                               
+const invertirNumeroComoTexto = numero => {
+    //El método toString  se utiliza para convertir un valor en una cadena de texto
+    const numeroInvertidoComoTexto = numero.toString().split("").reverse().join("");
+    //split indica que se dividirá en cada carácter individual.
+    //reverse Esta función invierte el orden de los elementos en el array.
+    //join indica que no se usará ningún separador entre los elementos al unirlos.
+    return numeroInvertidoComoTexto;
+};
 
+const invertirTexto = texto => {
+       //el split se utiliza para dividir una cadena de texto en un array de subcadenas utilizando un delimitador especificado.
+    const splitText = texto.split('');
+    //El método reverse se utiliza para invertir el orden de los elementos en un array
+    const reversedText = splitText.reverse();
+    //utilizamos el join para combinar todos los elementos de un array en una cadena de texto, separados por un delimitador especificado.
+    const joinedText = reversedText.join('');
+    return joinedText;
+};
 
-function mayusculas(jesus) {
-    return jesus.toUpperCase();
+const invertirArreglo = arreglo => {
+//Un arreglo se utiliza para almacenar y organizar múltiples valores en una sola variable
+    const reverseObject = arreglo.reverse();
+    return reverseObject;
+};
+
+const conversionDatos = (data) => {
+    //el tipeof lo utilizamos para determinar el tipo de datos de una variable o expresion
+    let dataType = typeof data;
+    let dataVar;
+//el if Permite controlar el flujo del programa y tomar decisiones basadas en condiciones booleanas (verdadero o falso).
+    if (data=== '' || data === null || data === undefined || data.length === 0) {
+        dataVar = 'El valor ingresado està vacio o incorrecto';
+        //La declaración "else"se utiliza junto con la declaración "if" para ejecutar un bloque de código alternativo cuando la condición del "if" no se cumple.
+    }else {
+        console.log(`el valor ingresado es de tipo: ${dataType},con un valor de: ${data}`);
+        //La declaración "switch"se utiliza para evaluar una expresión y ejecutar diferentes bloques de código según el valor de la expresión
+        switch (dataType) {
+            //el "case" se utiliza dentro de una declaración "switch"para definir los diferentes casos que se evaluarán.
+            case 'number':
+                dataVar = invertirNumero(data);
+                //el break se utiliza dentro de un bucle o una estructura de control (como "switch") para salir inmediatamente del bloque actual y continuar con la ejecución del código después del bloque.
+                break;
+                case 'string':
+                    dataVar = invertirTexto(data);
+                    break;
+                    case 'object':
+                        dataVar = invertirArreglo(data);
+                        break;
+                        // el default se utiliza para asignar un valor predeterminado a una variable si no se proporciona ningun valor
+                        default:
+                            datavar = 'el valor ingresado no se puede invertir';
+        }
+    }
+    return dataVar;
 }
-function minusculas(DAVID) {
-    return DAVID.toLowerCase();
-}
-function mayusculasyminusculas(jesus , david) {
-    return jesus[0].toUpperCase() + jesus.substring(1).toLowerCase() +"  " + david[0].toUpperCase() + david.substring(1).toLowerCase();
-}
 
+math.invertirNumero = invertirNumero;
+math.invertirNumeroComoCadena = invertirNumeroComoTexto;
+math.invertirTexto = invertirTexto;
+// se invierte un arreglo para cambiar el orden de los elementos del arreglo, de manera que el último elemento se convierta en el primer elemento
+math.invertirArreglo = invertirArreglo;
+math.conversionDatos = conversionDatos;
 
-
-
-
-functions.minusculas = minusculas;
-functions.mayusculas = mayusculas;
-functions.mayusculasyminusculas = mayusculasyminusculas;
-
-module.exports = functions;
+module.exports =math;
